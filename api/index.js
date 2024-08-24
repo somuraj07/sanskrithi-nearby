dd796235e395d2f38a08c301e13e73b8d24f6ba5
katika somasankar <somasankar33@gmail.com>
2024-08-24 19:52:58 +0530

    create a test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 const app = express();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,3 +14,6 @@ mongoose.connect(process.env.MONGO).then(() => {
 app.listen(3000, () => {
     console.log("server is running ")
 });
+
+
+app.use("/api/user",userRouter)
